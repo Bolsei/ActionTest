3c16eaeb770819277fdd98bf379d13fe94a4f2df
TimoRudersdorf <trudersdorf@integrationmatters.com>
2025-09-01 10:38:02 +0200

    add checkstyle error for test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,9 @@ package org.example;
  * Main Call
  */
 public class Main {
+
+    public static final int maxValue = 5;
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
